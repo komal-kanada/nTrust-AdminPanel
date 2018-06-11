79a6485bcaa4f77b74f1fe822ce131fbc3b5a5b6
Radha Kotecha <radha.kotecha@bacancytechnology.com>
2018-06-11 14:09:31 +0530

    Added Sub-Experience File

---
+++ src/pages/SubExperience/SubExperience.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class SubExperience extends Component {
+  render() {
+    return (
+      <div className="animated fadeIn">
+         In SubExperience page
+      </div>
+    )
+  }
+}
+
+export default SubExperience;
