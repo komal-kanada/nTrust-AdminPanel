348ac182a8fd9f950f3d2aa768366bcf21e699e3
Radha Kotecha <radha.kotecha@bacancytechnology.com>
2018-07-03 13:52:37 +0530

    Change in global.js.

--- src/common/global.js
+++ src/common/global.js
@@ -6,11 +6,4 @@ export const token = async () => {
     }
 };
 
-// server
-// export const API_BASE_URL = "http://158.69.205.234:3000/api/v1";
-// mihir
-// export const API_BASE_URL = "http://192.168.1.177:3000/api/v1";
-// heroku
 export const API_BASE_URL = "https://ntrust.herokuapp.com/api/v1";
-// radha
-// export const API_BASE_URL = "http://192.168.1.16:3000/api/v1";
